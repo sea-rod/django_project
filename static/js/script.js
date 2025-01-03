156bcf7182bd3e8455a283e5ff83d74a13fa72d8
sea-rod <seam22rod3@gmail.com>
2025-01-03 12:32:28 +0530

    inital commit

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+// Optional JavaScript for smooth animations or interactive elements
+// For example, adding a smooth scroll effect to "View Project" links
+
+document.querySelectorAll('.project-card a').forEach(item => {
+    item.addEventListener('click', event => {
+        // Here, you can add any action like navigating to the project details page
+        alert('Navigating to project details...');
+    });
+});
